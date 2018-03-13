1894637798ac26ed2878c3758acf0a14c5b39da0
Pedro <pedromiguel_7@live.com.pt>
2018-03-13 15:37:04 +0000

    Fichas Submetidas

---
+++ Fichas-da-aula/Ficha_aula1/Ficha1ex4pedirosnumeros.py
@@ -0,0 +1,7 @@
+raio = int(input(" Introduza o 1 valor: "))
+perimetro = int(input(" Introduza o 2 valor: "))
+pi = 3.14
+perimetro = (2 * (pi * raio))
+area = (pi * (raio * raio))
+print(" O resultado é: ", perimetro)
+print(" O resultado é: ", area)
