2b3c312fee74352f8cfa7a78335c756ed849a12f
Pedro <pedromiguel_7@live.com.pt>
2018-03-13 19:08:34 +0000

    Novo

--- Ficha_aula1/Ficha1ex3pedirosnumeros.py
+++
@@ -1,5 +0,0 @@
-comprimento = int(input("Valor de 1: "))
-largura = int(input("Valor de 2: "))
-perimetro = comprimento + comprimento + largura +largura
-area = comprimento * largura
-print(" O resultado é: ",perimetro,area) 
