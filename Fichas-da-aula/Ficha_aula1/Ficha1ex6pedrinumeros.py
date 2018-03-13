dab0d85d520c3cb2ce82dcabf3dd2fe4e5b5b0af
Pedro <pedromiguel_7@live.com.pt>
2018-03-13 18:45:50 +0000

    Ficha Submtida

---
+++ Fichas-da-aula/Ficha_aula1/Ficha1ex6pedrinumeros.py
@@ -0,0 +1,11 @@
+numero1 = int(input("Introduzir o primeiro valor: "))
+numero2 = int(input("Introduzir o segundo valor: "))
+numero3 = int(input("Introduzir o terceiro valor: "))
+numero4 = int(input("Introduzir o quarto valor: "))
+numero5 = int(input("Introduzir o quinto valor: "))
+Média = float( numero1 + numero2 + numero3 + numero4 + numero5 )/5
+Parteinteira = int( numero1 + numero2 + numero3 + numero4 + numero5)//5
+Modulodedivisao = ( numero1 + numero2 + numero3 + numero4 + numero5)%100
+print(" O resultado é: ", Média)
+print(" O resultado é: ", Parteinteira)
+print(" O resultado é: ", Modulodedivisao)
