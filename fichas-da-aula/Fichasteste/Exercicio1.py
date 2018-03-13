ff479848fe2c6897ee6a53afdfab8ef7c9be9618
Pedro <pedromiguel_7@live.com.pt>
2018-03-13 19:24:13 +0000

    Exercicio teste

--- fichas-da-aula/Fichasteste/Exercicio1.py
+++ fichas-da-aula/Fichasteste/Exercicio1.py
@@ -1,4 +1,4 @@
 numero = int(input(" Escolha um numero: "))
-antecedor = numero -1 
+antecedor = numero - 1 
 sucessor = numero + 1
 print(" O antecedor de {} é {} e o sucessor é {}".format(numero,antecedor,sucessor))
