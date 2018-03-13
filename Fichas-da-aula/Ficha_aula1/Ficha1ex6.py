a9a71665af98854c769e9f3b93c0cf8e506474c7
Pedro <pedromiguel_7@live.com.pt>
2018-03-13 16:17:54 +0000

    Fichas Submetidas

---
+++ Fichas-da-aula/Ficha_aula1/Ficha1ex6.py
@@ -0,0 +1,11 @@
+numero1 = 10
+numero2 = 5
+numero3 = 2
+numero4 = 4
+numero5 = 6
+Média = float( numero1 + numero2 + numero3 + numero4 + numero5 )/5
+Parteinteira = int( numero1 + numero2 + numero3 + numero4 + numero5)//5
+Modulodedivisao = ( numero1 + numero2 + numero3 + numero4 + numero5)%100
+print(" O resultado é: ", Média)
+print(" O resultado é: ", Parteinteira)
+print(" O resultado é: ", Modulodedivisao)
