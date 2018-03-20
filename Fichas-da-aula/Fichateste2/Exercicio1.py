0b85f655b283ac1af0aa5bef526e351dc7d20bd8
Pedro <pedromiguel_7@live.com.pt>
2018-03-20 15:46:06 +0000

    Novas Fichas

--- Fichas-da-aula/Fichateste2/Exercicio1.py
+++ Fichas-da-aula/Fichateste2/Exercicio1.py
@@ -2,10 +2,9 @@ Nota1 = float(input( " Primeira Nota: "))
 Nota2 = float(input( " Segunda Nota: "))
 Nota3 = float(input( " Terceira Nota: "))
 
-Médiafinal = (Nota1 * 25 + Nota2 * 35 + Nota3 * 40) 
+Médiafinal = ((Nota1 * 25 + Nota2 * 35 + Nota3 * 40)/100)
 
 if Médiafinal >= 9.5:
-    print( " O aluno está aprovado")
+    print(" O aluno está aprovado")
 else:
         print(" O aluno está reprovado")
-            
