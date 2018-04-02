8a93aaeb17560831c2ea518ed22f0fc98ca5bafd
Pedro <pedromiguel_7@live.com.pt>
2018-04-02 11:19:38 +0100

    novas fichas

--- Fichas-da-aula/Fichateste2/Exercicio4.py
+++ Fichas-da-aula/Fichateste2/Exercicio4.py
@@ -3,4 +3,4 @@ Numero1 = int(input(" Digite o seu numero: "))
 if Numero1 % 3 == 0 and Numero1 % 5 == 0:
     print(" O seu numero {} é divisivel por 3 e por 5".format(Numero1))
 else:
-    print(" O seu numero {} não é divisivel por 3 e por 5".format(Numero1))
+    print(" O seu numero {} não é divisivel por 3 nem por 5".format(Numero1))
