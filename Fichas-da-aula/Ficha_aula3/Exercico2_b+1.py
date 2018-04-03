7de96af9bd75b321e0615eabd0c05edb2227d6af
Pedro <pedromiguel_7@live.com.pt>
2018-04-03 10:18:52 +0100

    Ficha pratica

--- Fichas-da-aula/Ficha_aula3/Exercico2_b+1.py
+++ Fichas-da-aula/Ficha_aula3/Exercico2_b+1.py
@@ -1,5 +1,5 @@
 a = int(input('Introduza o inicio do intervalo: '))
 b = int(input('Introduza o fim do intervalo: '))
 
-for item in range(a, b, 1) :
+for item in range(a, b+1, 1) :
     print(item)
