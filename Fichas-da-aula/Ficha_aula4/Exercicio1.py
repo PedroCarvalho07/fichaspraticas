d72d277843bbf893eaba218c3718080b316bdc29
Pedro <pedromiguel_7@live.com.pt>
2018-04-03 15:41:41 +0100

    novas

--- Fichas-da-aula/Ficha_aula4/Exercicio1.py
+++ Fichas-da-aula/Ficha_aula4/Exercicio1.py
@@ -1,4 +1,4 @@
-numero = int(input(" Introduza um numero inteiro :"))
+numero = int(input(" Introduza um numero inteiro : "))
 def numero_inteiro( numero):
     print ( '*' * numero) 
 numero_inteiro(numero)
