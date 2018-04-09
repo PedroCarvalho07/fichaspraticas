0deef9a68704d6dc1231ff6085e5c98eae47f3a4
Pedro <pedromiguel_7@live.com.pt>
2018-04-09 19:44:54 +0100

    Estudo

---
+++ Fichas-da-aula/Treinar_em_casa/Ficha1_ex1.py
@@ -0,0 +1,5 @@
+numero_1 = int(input(" Escolha o primeiro numero que deseja somar: "))
+numero_2 = int(input(" Escolha o segundo numero que deseja somar: "))
+
+soma = numero_1 + numero_2
+print (" O resultado é : ", soma)
---
+++ Fichas-da-aula/Treinar_em_casa/Ficha1_ex2.py
@@ -0,0 +1,6 @@
+numero_1 = int(input(" Escolha o primeiro numero :"))
+numero_2 = int(input(" Escolha o segundo numero : "))
+
+subrtração = numero_1 - numero_2
+multiplicação = subrtração * numero_1
+print (" O seu resultado é : ", multiplicação)
