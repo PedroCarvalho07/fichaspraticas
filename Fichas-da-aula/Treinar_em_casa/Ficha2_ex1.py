99298f6ce7f0cdfe64bd7302c793797dbbc86c7d
Pedro <pedromiguel_7@live.com.pt>
2018-04-10 11:40:03 +0100

    Estudo

---
+++ Fichas-da-aula/Treinar_em_casa/Ficha2_ex1.py
@@ -0,0 +1,7 @@
+num_1 = int(input(" Escolha o seu primeiro numero : "))
+num_2 = int(input(" Escolha o seu segundo numero : "))
+
+if num_1 > num_2:
+    print (num_1)
+else :
+    print (num_2)
---
+++ Fichas-da-aula/Treinar_em_casa/Ficha2_ex2.py
@@ -0,0 +1,9 @@
+num_1 = int(input( " Escolha o primeiro numero : "))
+num_2 = int(input( " Escolha o segundo numero : "))
+
+if num_1 > num_2 :
+    print ( num_1,num_2 )
+elif num_2 > num_1 :
+    print ( num_2,num_1 )
+elif num_1 == num_2 :
+    print ( " Os numeros são iguais ")
